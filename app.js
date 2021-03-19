883276863863d85a7ad02703a4c5257bd2e060ea
u37314kd <krzysztof.dziuba@student.manchester.ac.uk>
2021-03-20 00:21:47 +0100

    adding basic routing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //server setup
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,3 +8,7 @@ const server = app.listen(8000, () => {
     console.log('listening to requests at 8000');
 });
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
