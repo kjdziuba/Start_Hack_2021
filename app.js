001e45e7334ffe8a5e9c935d5fe1bd5e28042a4f
u37314kd <krzysztof.dziuba@student.manchester.ac.uk>
2021-03-20 00:24:36 +0100

    changing the port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const server = app.listen(8000, () => {
+const server = app.listen(80, () => {
     console.log('listening to requests at 8000');
 });
 
