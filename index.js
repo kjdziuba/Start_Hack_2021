009ca8f17f8d394d16e7ca5162e880b4c963c003
u37314kd <krzysztof.dziuba@student.manchester.ac.uk>
2021-03-20 14:26:27 +0100

    added registering with db

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const session = require('express-session');
 const flash = require('express-flash');
 const bcrypt = require('bcrypt');
 
-const initialize = require('./passport-config');
-
-initialize(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
-
 const app = express();
 
 const server = app.listen(8081, () => {
@@ -35,10 +27,43 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+const initializePassport = require('./passport-config');
+initializePassport(passport);
+
 //static files
 app.use(express.static('public'));
 
-const users = []
+
+//db set up
+var con = mysql.createConnection({
+    host: "***REMOVED***",
+    user: "***REMOVED***",
+    password: "***REMOVED***",
+    database: "somedb",
+    port: '3306',
+    ssl: true
+});
+
+
+con.connect(function (err) {
+    if (err) {
+        console.log("!!! Cannot connect !!! Error:");
+        console.log(err.stack)
+    }
+    else {
+        console.log("Connection established.");
+        let sql = 'CREATE TABLE users (ID int, email varchar(255), username varchar(255), password varchar(255) );';
+        con.query(sql, function (err, result) {
+            if (err) {
+                //console.log(err);
+                console.log("table already exists")
+            }
+            else { console.log("Table created"); }
+        });
+    }
+});
+
+
 
 
 app.get('/', (req, res) => {
@@ -54,42 +79,65 @@ app.get('/register', (req, res) => {
 });
 
 
-
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
+app.post('/register',async (req,res) =>{
+    try{
+        var data = req.body;
+        id = Date.now().toString()
+        console.log(data)
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+        var sql = `INSERT INTO users (ID, email, username, password) VALUES (${id},'${data.email}', '${data.username}', '${hashedPassword}')`;
+        console.log(sql)
+        con.query(sql, function (err, result) {
+            if(err){
+                if(err.code="ER_DUP_ENTRY"){
+                    res.send("<script>alert('It looks like that email already exists! Please use a different email, or log in');window.location.replace(window.location.href);</script>");
+                }else{console.log(err);
+                    res.send("<script>alert('An error occurred! Yell aggressively at the developers!');window.location.replace(window.location.href);</script>");
+                }
+                
+            }else{
+                console.log("1 record inserted");
+                res.redirect('/');
+            }
+        });
+    }catch{
+        res.redirect('/register');
     }
-  })
-  
+});
 
-  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
-  }))
+}))
+
+app.get('/getUsers', (req, res) => {
+    var sql = `SELECT * FROM users`;
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) {
+            //console.log(err);
+            console.log("error")
+        }
+        else { 
+            console.log(result); 
+        }
+    });
+})
 
 
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-      return res.redirect('/')
+        return res.redirect('/')
     }
     next()
-  }
+}
 
-  function checkAuthenticated(req, res, next) {
+function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-      return next()
+        return next()
     }
-  
+
     res.redirect('/login')
-  }
+}
--- passport-config.js
+++ passport-config.js
@@ -1,29 +1,73 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const mysql = require('mysql');
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
-    if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
-    }
+var con = mysql.createConnection({
+  host: "***REMOVED***",
+  user: "***REMOVED***",
+  password: "***REMOVED***",
+  database: "somedb",
+  port: '3306',
+  ssl: true
+});
 
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        return done(null, user)
-      } else {
-        return done(null, false, { message: 'Password incorrect' })
-      }
-    } catch (e) {
-      return done(e)
-    }
-  }
+function initialize(passport) {
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user.id))
+  passport.use(new LocalStrategy(
+    {
+      usernameField: 'email'
+    }, (email, password, done) => {
+      console.log('called local strategy');
+
+      con.connect((err, client) => {
+        console.log('called local strategy - in db connection');
+
+        var user = {};
+        var sql = `SELECT * FROM users WHERE email = '${email}'`;
+        console.log(sql);
+
+        con.query(sql, async function(err, result){
+            if(err) console.log(err);
+            else if(result.length != 0){ 
+                console.log(result[0]);
+                user = result[0];
+            }
+            else{
+              return done(null, false, { message: 'No user with that email' })
+            }
+            try {
+              if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+              } else {
+                return done(null, false, { message: 'Password incorrect' })
+              }
+            } catch (e) {
+              return done(e)
+            }
+        })
+      });
+
+    }));
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
   passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
+    console.log('called deserializeUser method');
+    con.connect((err, client) => {
+      var user = {};
+      console.log('called deserializeUser - in db connection');
+      var sql = `SELECT * FROM users WHERE id = ${id}`;
+      console.log(sql);
+      con.query(sql, function(err, result){
+        if(err) console.log(err);
+        else if(result.length != 0){ 
+          console.log(result[0]);
+          user = result[0];
+          done(null, user);
+      }
+        
+      });
+    });
+  });
 }
-
 module.exports = initialize
