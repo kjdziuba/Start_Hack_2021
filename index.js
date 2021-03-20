0a4fff4f52f5c8e95e2a80c9b9a2e695c705f4fe
u37314kd <krzysztof.dziuba@student.manchester.ac.uk>
2021-03-20 01:52:19 +0100

    setting up db

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 //server setup
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql')
 
 const app = express();
 
 const server = app.listen(8080, () => {
     console.log('listening to requests at 8000');
+});
+
+
+let con = mysql.createConnection({
+    host: "stewarts.database.windows.net",
+    port: 1433,
+    user: "krzysztof",
+    password: "lots$redBulls",
+    database: "starthack_2021",
+    connectTimeout: 30000
+});
+
+
+con.connect(function (err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to MySQL!");
+        con.query("CREATE DATABASE mydb", function (err, result) {
+            if (err) { console.log("Database already exists"); }
+            else { console.log("Database created"); }
+        });
+
+        sql = "CREATE TABLE users (id VARCHAR(255), fname VARCHAR(255), lname VARCHAR(255), email VARCHAR(255), password VARCHAR(255), display_name VARCHAR(255))";
+        con.query(sql, function (err, result) {
+            if (err) {
+                //console.log(err);
+                console.log("table already exists")
+            }
+            else { console.log("Table created"); }
+        });
+    }
 });
 
 
